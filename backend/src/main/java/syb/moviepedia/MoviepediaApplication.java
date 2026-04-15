140408cf42cc7729139606c01c9717a05cc0a9c3
SYB <yoon6669@naver.com>
2026-04-15 22:57:00 +0900

    backend: 첫번째 커밋

---
+++ backend/src/main/java/syb/moviepedia/MoviepediaApplication.java
@@ -0,0 +1,13 @@
+package syb.moviepedia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviepediaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviepediaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/syb/moviepedia/MoviepediaApplicationTests.java
@@ -0,0 +1,13 @@
+package syb.moviepedia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviepediaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
