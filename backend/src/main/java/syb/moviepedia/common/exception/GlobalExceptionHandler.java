3c54bf7b363c35b993f1730fb906e665ebf06a7b
SYB <yoon6669@naver.com>
2026-04-20 19:01:57 +0900

    backend: @Valid 검증 예외 ControllerAdvice에서 처리 및 MemberController의 BindingReulst 파라미터 삭제

--- backend/src/main/java/syb/moviepedia/common/exception/GlobalExceptionHandler.java
+++ backend/src/main/java/syb/moviepedia/common/exception/GlobalExceptionHandler.java
@@ -3,14 +3,22 @@ package syb.moviepedia.common.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
+    // 회원 가입 필드 중복 검사 예외
     @ExceptionHandler(SignupFieldException.class)
     public ResponseEntity<String> handleSignupFieldException(SignupFieldException e) {
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+    // @Valid 검증 예외
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<String> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        // TODO: 추후 bindingResult 처리하기 및 SignupFieldException와 함께 처리할 방법 고민해보기
         return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
     }
 }
--- backend/src/main/java/syb/moviepedia/member/controller/MemberController.java
+++ backend/src/main/java/syb/moviepedia/member/controller/MemberController.java
@@ -24,19 +24,19 @@ public class MemberController {
     final private MemberService memberService;
 
     @PostMapping("/signup")
-    public ResponseEntity<Void> signup(@Valid @RequestBody MemberDto dto, BindingResult bindingResult) {
+    public ResponseEntity<Void> signup(@Valid @RequestBody MemberDto dto) {
 
-        // 검증 실패시
-        // 필드 입력 조건에 대한 검증을 하는 것임. 아이디, 비밀번호, 닉네임 등의 중복 체크는 하지 않음)
-        if (bindingResult.hasErrors()) {
-            Set<String> errors = new HashSet<>(); // 필드명, 에러코드 작성
-            bindingResult.getFieldErrors().forEach(error -> {
-                log.info("error: " + error);
-                errors.add(error.getField()); // 에러 필드명과 필드에 대한 기본 에러 메시지 넣음
-            });
-            // 400 응답. 프론트에서 처리.
-            return ResponseEntity.badRequest().build();
-        }
+//        // 검증 실패시
+//        // 필드 입력 조건에 대한 검증을 하는 것임. 아이디, 비밀번호, 닉네임 등의 중복 체크는 하지 않음)
+//        if (bindingResult.hasErrors()) {
+//            Set<String> errors = new HashSet<>(); // 필드명, 에러코드 작성
+//            bindingResult.getFieldErrors().forEach(error -> {
+//                log.info("error: " + error);
+//                errors.add(error.getField()); // 에러 필드명과 필드에 대한 기본 에러 메시지 넣음
+//            });
+//            // 400 응답. 프론트에서 처리.
+//            return ResponseEntity.badRequest().build();
+//        }
         log.info("signup 호출");
         // 중복 검사 및 가입
         memberService.signup(dto);
