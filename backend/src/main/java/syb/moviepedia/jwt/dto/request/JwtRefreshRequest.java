7e181119972dd5f016f56979d17b302a1389d260
SYB <yoon6669@naver.com>
2026-05-23 10:44:40 +0900

    FE: 코멘트 목록 초과 내용 말줌임표(ellipsis) 처리

--- backend/src/main/java/syb/moviepedia/jwt/dto/request/JwtRefreshRequest.java
+++ backend/src/main/java/syb/moviepedia/jwt/dto/request/JwtRefreshRequest.java
@@ -1,4 +1,4 @@
-package syb.moviepedia.jwt.dto;
+package syb.moviepedia.jwt.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
@@ -8,7 +8,7 @@ import lombok.Getter;
  * 액세스 토큰 재발급을 위해 그에 필요한 리프레쉬 토큰을 전달받음
  */
 @Getter
-public class JwtRefreshRequestDto {
+public class JwtRefreshRequest {
     @NotBlank
     private String refreshToken;
 }
